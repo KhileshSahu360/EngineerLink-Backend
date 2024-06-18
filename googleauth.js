7779be9500d942ba4d951137056c23d8730ecd44
KhileshSahu360 <khileshanjole@gmail.com>
2024-06-18 09:07:05 +0900

    cookie changes

--- googleauth.js
+++ googleauth.js
@@ -64,7 +64,12 @@ googleRouter.get('/google/callback', passport.authenticate('google', { failureRe
         const token = Generatejsonwebtoken(payLoad);
         const expiryDate = new Date();
         expiryDate.setDate(expiryDate.getDate() + 30);
-        res.cookie('token',token,{expires:expiryDate})
+        res.cookie('token',token,{
+          expires:expiryDate,
+          domain: `${frontend_url}`, // specify your frontend domain
+          secure: true, // cookie is only sent over HTTPS
+          sameSite: 'Strict' // or 'Lax' or 'None'
+        })
       } else {
         console.log(error)
         return res.redirect(`${frontend_url}servererror`);
